70ba185d449d1e4ae5f6d6d72a44f8ba7443780e
Michał Klich <michal.klich@netguru.com>
2022-02-25 12:12:56 +0100

    Launchdarkly

--- app/main.py
+++ app/main.py
@@ -1,20 +1,42 @@
-from typing import Optional
+import os
 
+import ldclient
 from fastapi import FastAPI
+from ldclient.config import Config
 
 app = FastAPI()
 
+store_id = os.environ.get("STORE_ID")
+environment = os.environ.get("ENV")
+sdk_key = os.environ.get("SDK_KEY")
+ldclient.set_config(Config(sdk_key))
+client = ldclient.get()
 
 @app.get("/")
 def read_root():
-	# if global flag
-	# return disabled
+    
+    # anonymous user
+    if client.variation("exit-sms-feature", {"key": "aa0ceb", "custom": {"store_id": store_id}}, False):
+        exit_sms_feature = "enabled"
+    else: 
+        exit_sms_feature = "disabled"
 
-	# if store flag
-	# return disabled store
-    return {"Hello": "World"}
+    # known user
+    if client.variation("new-payment-system", {"key": "123", "custom": {"store_id": store_id}}, False):
+        payment_system = "new"
+    else:
+        payment_system = "old"
 
+    # known user
+    if client.variation("widget-generator", {"key": "456", "custom": {"store_id": store_id}}, False):
+        widget_generator = "enabled"
+    else:
+        widget_generator = "disabled"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {
+        "env": environment,
+        "store_id": store_id,
+        "exit-sms-feature": exit_sms_feature,
+        "payment-system": payment_system,
+        "widget-generator": widget_generator
+        }
